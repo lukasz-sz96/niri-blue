7d7a18215fa9b2638de003515c674466fe384fe7
Łukasz <lukasz.szczesny96@gmail.com>
2026-05-15 12:42:07 +0200

    install niri and noctalia packages

---
+++ build/20-niri-noctalia.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -oue pipefail
+
+# Terra repo for Noctalia
+dnf5 install -y --nogpgcheck \
+  --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' \
+  terra-release
+
+# Niri + Noctalia + useful Niri desktop bits
+dnf5 install -y \
+  niri \
+  xwayland-satellite \
+  noctalia-shell \
+  xdg-desktop-portal-gnome \
+  xdg-desktop-portal-gtk \
+  fuzzel \
+  alacritty \
+  wl-clipboard \
+  cliphist \
+  brightnessctl \
+  pavucontrol \
+  blueman \
+  wlsunset \
+  qt6ct \
+  nwg-look \
+  gnome-keyring \
+  seahorse
